03a7342fa988628186c854f22979e12c1c0ce2f6
Eduard Melik-Hakobyan <eduard.wintersnow@outlook.com>
2025-01-18 10:58:40 +0400

    Add project files.

---
+++ TheFirstPyApp/TheFirstPyApp.py
@@ -0,0 +1,22 @@
+# print method
+print("The first line")
+print("The second line")
+
+# String variable
+firstName = "Eduard"
+fruit = "Pineapple"
+
+# Integer variable
+age = 22
+
+# Float variable
+height = 1.7
+
+# Boolean variable
+isStudent = False
+
+# if else statement
+if isStudent:
+    print("You are a student")
+else:
+    print("You are not a student")
