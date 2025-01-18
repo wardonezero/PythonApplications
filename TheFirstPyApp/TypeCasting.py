9f85562470ad47832c2983d683d3afb3d76382ea
Eduard Melik-Hakobyan <eduard.wintersnow@outlook.com>
2025-01-18 11:18:29 +0400

    Type casting

---
+++ TheFirstPyApp/TypeCasting.py
@@ -0,0 +1,22 @@
+# Type casting
+
+# Explicit type casting
+name = 'Eduard'
+age = 22
+gpa = 4.2
+student = False
+
+print(type(name))
+
+age = float(age)
+print(age)
+
+gpa = int(gpa)
+print(gpa)
+
+age = bool(age)
+print(age)
+
+# Implicit type casting
+x = 3/2
+print(x)
