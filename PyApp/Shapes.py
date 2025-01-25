d964b6374ca877717b84131a7175cfc45f82faa1
Eduard Melik-Hakobyan <eduard.wintersnow@outlook.com>
2025-01-25 10:06:41 +0400

    fix and abstract

--- PyApp/Shapes.py
+++ PyApp/Shapes.py
@@ -7,7 +7,10 @@ class Shape:
     
     def describe(self):
         print(f"It is {self.color} and {'filled' if self.isFilled else 'not filled'}")
-        
+    
+    @abstractmethod
+    def area(self):
+        pass
 
 class Triangle(Shape):
     def __init__(self, color, isFilled, base, height):
@@ -16,8 +19,11 @@ class Triangle(Shape):
         self.height = height
 
     def describe(self):
-        print(f"It is a triangle with an area of {self.base * self.height / 2}cm^2")
+        print(f"It is a triangle with an area of {self.area()}cm^2")
         super().describe()
+    
+    def area(self):
+        return self.base * self.height / 2
 
 class Circle(Shape):
     def __init__(self, color, isFilled, radius):
@@ -25,8 +31,11 @@ class Circle(Shape):
         self.radius = radius
     
     def describe(self):
-        print(f"It is a circle with an area of {3.14 * self.radius * self.radius}cm^2")
+        print(f"It is a circle with an area of {self.area()}cm^2")
         super().describe()
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
 
 class Square(Shape):
     def __init__(self, color, isFilled, side):
@@ -34,5 +43,8 @@ class Square(Shape):
         self.side = side
 
     def describe(self):
-        print(f"It is a square with an area of {self.width * self.width}cm^2")
+        print(f"It is a square with an area of {self.area}cm^2")
         super().describe()
+
+    def area(self):
+        return self.side * self.side
