646b55fb416b0920288a1578b1cba08299ff2755
Eduard Melik-Hakobyan <eduard.wintersnow@outlook.com>
2025-01-27 11:51:52 +0400

    MULTITHREADING

---
+++ PyApp/MULTITHREADING(Chores).py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+def walk_dog(first, second):
+    time.sleep(6)
+    print("You finish walking {first} and {second}")
+
+def take_out_trash():
+    time.sleep(2)
+    print("You take out the trash")
+
+def get_mail():
+    time.sleep(4)
+    print("You get the mail")
+
+chore1 = threading. Thread(target=walk_dog, args=('Garfield', 'Odie'))
+chore1. start()
+
+chore2 = threading. Thread(target=take_out_trash)
+chore2. start()
+
+chore3 = threading. Thread(target=get_mail)
+chore3. start()
+
+chore1.join()
+chore2.join()
+chore3.join()
+
+print("All chores are done")
