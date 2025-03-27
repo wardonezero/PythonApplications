0e3fc378e1fa6d0be8989a177c72b885d1b50af5
wardonezero <eduard.wintersnow@outlook.com>
2025-03-27 20:47:50 +0400

    home work

---
+++ PicsartAcademy/HomeWorks/homework24_27.py
@@ -0,0 +1,86 @@
+# 24/27
+
+from collections.abc import Iterable
+from functools import reduce
+
+# 1. Implementing filter
+
+
+def customFilter(function, iterable):
+    if not hasattr(iterable, "__iter__"):
+        raise TypeError(f"{type(iterable)} object is not iterable")
+    if not callable(function) and function is not None:
+        raise TypeError(f"{type(function)} object is not callable")
+    if function == None:
+        return [item for item in iterable if item]
+    return [item for item in iterable if function(item)]
+
+
+# 2. Implementing map
+
+
+def customMap(function, *iterables):
+    if not callable(function):
+        raise TypeError(f"{type(function)} object is not callable")
+    for iterable in iterables:
+        if not isinstance(iterable, Iterable):
+            raise TypeError(f"{type(iterable)} object is not iterable")
+    minLen = len(min(iterables, key=len))
+    for i in range(minLen):
+        yield function(*[iterable[i] for iterable in iterables])
+
+
+# 3. Implementing zip
+
+
+def customZip(*iterables):
+    for iterable in iterables:
+        if not isinstance(iterable, Iterable):
+            raise TypeError(f"{type(iterable)} object is not iterable")
+    minLen = len(min(iterables, key=len))
+    for i in range(minLen):
+        yield tuple([iterable[i] for iterable in iterables])
+
+
+# 4. Implementing reduce
+
+
+def customReduce(function, iterable, initial=None):
+    if not callable(function):
+        raise TypeError(f"{type(function)} object is not callable")
+    if not isinstance(iterable, Iterable):
+        raise TypeError(f"{type(iterable)} object is not iterable")
+    if initial is not None:
+        result = initial
+        for item in iterable:
+            result = function(result, item)
+    else:
+        result = iterable[0]
+        for item in iterable[1:]:
+            result = function(result, item)
+    return result
+
+
+# 5. Implementing enumerate
+
+def customEnumerate(iterable, start=0):
+    if not isinstance(iterable, Iterable):
+        raise TypeError(f"{type(iterable)} object is not iterable")
+    if not isinstance(start, int):
+        raise TypeError(f"{type(start)} object is not int")
+    for i in range(len(iterable)):
+        yield (start + i, iterable[i])
+
+if __name__ == "__main__":
+    print(" filter", tuple(filter(None, "abc")))
+    print("Cfilter", customFilter(None, "abc"))
+    print(" map", tuple(map(lambda x, y: x + y, "abc", "123")))
+    print("Cmap", tuple(customMap(lambda x, y: x + y, "abc", "123")))
+    print(" zip", tuple(zip("abc", "123")))
+    print("Czip", tuple(customZip("abc", "123")))
+    print(" reduce", reduce(lambda x, y: x + y, [1, 2, 3, 4, 5, 6]))
+    print("Creduce", customReduce(lambda x, y: x + y, [1, 2, 3, 4, 5]))
+    print(" enumerate", tuple(enumerate("abc")))
+    print(" enumerate", tuple(enumerate("abc",-1)))
+    print(" enumerate", tuple(customEnumerate("abc",-1)))
+    print(" enumerate", tuple(customEnumerate("abc")))
